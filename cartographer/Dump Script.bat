945c95137f2bd1bc4fe29c8d432fd4717154aa0e
Normmatt <normmatt234@gmail.com>
2012-02-15 13:26:22 +1300

    Add basic script dumper

---
+++ cartographer/Dump Script.bat
@@ -0,0 +1,7 @@
+cartographer ../rom/input.gba gurulogic_commands.txt gurulogic_script -s
+cartographer ../rom/input.gba gurulogic_commands.txt gurulogic_script -m
+
+cartographer "../Guru Logic Champ (Japan)_Partial Translation.gba" gurulogic_commands.txt gurulogic_script_partial -s
+cartographer "../Guru Logic Champ (Japan)_Partial Translation.gba" gurulogic_commands.txt gurulogic_script_partial -m
+
+pause

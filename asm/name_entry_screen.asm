b16f7405459abb427994616b77dd7d0d8e6eeaf1
moozilla <evilgeniuscow@gmail.com>
2012-02-25 07:32:32 -0800

    started name entry modifications - it now starts on ABC instead of kana, and kana pages are disabled. working on getting rid of arrow sprites.

---
+++ asm/name_entry_screen.asm
@@ -0,0 +1,28 @@
+ ; Guru Logic Champ - Name Entry Screen tweaks
+ ; by Spikeman
+
+.gba
+.open "rom/output.gba",0x08000000
+.thumb
+
+; first, load the graphics for ABC input instead of kana:
+
+; load ABC tiles instead of hirigana tiles
+.org 0x08017AD4
+	.dcd 0x082779C4 ; change ldr at 80178E0
+
+; load ABC tilemap instead of hirigana tilemap
+.org 0x08017B04
+	.dcd 0x083848FC ; change ldr at 801793A
+	
+; make it so we're always on the ABC screen as far as typing is concerned
+.org 0x0801814C
+	mov r2, 2	; hirigana = 0, katakana = 1, ABC = 2; was a ldrh
+	
+; disable changing of pages
+.org 0x08017FDC
+	b 0x080180C4	; was beq if r5 = 0 (r5 = 1 or -1 if changing page)
+
+.close
+
+ ; make sure to leave an empty line at the end
--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 copy rom\input.gba rom\output.gba
 armips.exe asm/HWF.asm
+armips.exe asm/name_entry_screen.asm
 Atlas rom\output.gba script\gurulogic_script_000.txt
 Atlas rom\output.gba script\gurulogic_script_001.txt
 pause

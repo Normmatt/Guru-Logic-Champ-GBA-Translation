9591c2175eb2194cd85c1f29ad1f0e84bc1612c1
Normmatt <normmatt234@gmail.com>
2012-02-28 22:41:06 +1300

    update script dumping commands and the build script to add the new script dump.

--- build.bat
+++ build.bat
@@ -3,4 +3,5 @@ armips.exe asm/HWF.asm
 armips.exe asm/name_entry_screen.asm
 Atlas rom\output.gba script\gurulogic_script_000.txt
 Atlas rom\output.gba script\gurulogic_script_001.txt
+Atlas rom\output.gba script\gurulogic_script_002.txt
 pause

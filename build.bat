2e9bca2bf67e0211bc9a356000264c8fd8ef7f2e
moozilla <evilgeniuscow@gmail.com>
2012-03-05 18:02:36 -0800

    compiled font_inserter and included windows exe (to compile with linux, latest haskell platform should be used)

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ copy rom\input.gba rom\output.gba
 armips.exe asm/HWF.asm
 armips.exe asm/name_entry_screen.asm
 armips.exe asm/cutscene_font.asm
-runghc "tools\font dumper\font_insert.hs" gfx\cutscene_font_eng.gba rom\output.gba
+font_insert gfx\cutscene_font_eng.gba rom\output.gba
 
 Atlas rom\output.gba script\gurulogic_script_000.txt
 Atlas rom\output.gba script\gurulogic_script_001.txt
